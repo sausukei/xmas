aa3f61b48879c990eb700210b1c5f434d65fa4a2
Ikeno Sosuke <ikenosousuke@icloud.com>
2024-12-20 10:40:45 +0900

    ああ

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/docs',
+  site: 'https://sausukei.github.io',
+  base: '/xmas',
 });
 
 
